f3fd315bf7f0a198daa58f1d6090b1830b16d612
123mame <61684834+123mame@users.noreply.github.com>
2022-08-25 16:38:47 +0300

    binary_trees v.1

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,142 +1,54 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_height - Function that measures the height of a binary tree
- * @tree: tree to go through
- * Return: the height
+ * count_nodes - function that finds the sibling of a node
+ *
+ * @root: val
+ * Return: Return the height of the tree
  */
-
-size_t binary_tree_height(const binary_tree_t *tree)
+int count_nodes(binary_tree_t *root)
 {
-	size_t l = 0;
-	size_t r = 0;
-
-	if (tree == NULL)
-	{
+	if (!root)
 		return (0);
-	}
-	else
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (tree->parent != NULL);
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		}
-		return ((l > r) ? l : r);
-		}
+	return (1 + count_nodes(root->left) + count_nodes(root->right));
 }
-
 /**
- * binary_tree_balance - Measures balance factor of a binary tree
- * @tree: tree to go through
- * Return: balanced factor
+ * is_max_heap - function that finds the sibling of a node
+ *
+ * @root: tree
+ * @index: max
+ * @n: val
+ * Return: Return the height of the tree
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int is_max_heap(binary_tree_t *root, int index, int n)
 {
-	int right = 0, left = 0, total = 0;
-
-	if (tree)
-	{
-		left = ((int)binary_tree_height(tree->left));
-		right = ((int)binary_tree_height(tree->right));
-		total = left - right;
-	}
-	return (total);
-}
-
-/**
- * tree_is_perfect - function that says if a tree is perfect or not
- * it has to be the same quantity of levels in left as right, and also
- * each node has to have 2 nodes or none
- * @tree: tree to check
- * Return: 0 if is not a perfect or other number that is the level of height
- */
-int tree_is_perfect(const binary_tree_t *tree)
-{
-	int l = 0, r = 0;
-
-	if (tree->left && tree->right)
-	{
-		l = 1 + tree_is_perfect(tree->left);
-		r = 1 + tree_is_perfect(tree->right);
-		if (r == l && r != 0 && l != 0)
-			return (r);
+	if (!root)
 		return (0);
-	}
-	else if (!tree->left && !tree->right)
-	{
+	if (index >= n)
+		return (0);
+	if (!root->left && !root->right)
 		return (1);
-	}
-	else
-	{
+	if ((root->left && root->left->n > root->n) ||
+			(root->right && root->right->n > root->n))
 		return (0);
-	}
+	if (root->left && !root->right)
+		return (is_max_heap(root->left, index * 2 + 1, n));
+	return (is_max_heap(root->left, index * 2 + 1, n) &&
+			is_max_heap(root->right, index * 2 + 2, n));
 }
-
 /**
- * binary_tree_is_perfect - perfect or not a tree
- * @tree: tree to check
- * Return: 1 is it is or 0 if not
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int result = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		result = tree_is_perfect(tree);
-		if (result != 0)
-		{
-			return (1);
-		}
-		return (0);
-	}
-}
-
-/**
- * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
- * @tree: tree to check
- * Return: 1 is it is or 0 if not
+ * binary_tree_is_heap - function that finds the sibling of a node
+ *
+ * @tree: tree
+ * Return: Return the height of the tree
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	int bval;
+	int nodes;
+	binary_tree_t *root = NULL;
 
-	if (tree == NULL)
-	{
+	if (!tree)
 		return (0);
-	}
-	if (tree->left && tree->left->n > tree->n)
-	{
-		return (0);
-	}
-	if (tree->right && tree->right->n > tree->n)
-	{
-		return (0);
-	}
-	if (binary_tree_is_perfect(tree))
-	{
-		return (1);
-	}
-	bval = binary_tree_balance(tree);
-	if (bval == 0)
-	{
-		return (binary_tree_is_perfect(tree->left)
-			&& binary_tree_is_heap(tree->right));
-	}
-	if (bval == 1)
-	{
-		return (binary_tree_is_heap(tree->left)
-			&& binary_tree_is_perfect(tree->right));
-	}
-	else
-	{
-		return (0);
-	}
+	root = (binary_tree_t *)tree;
+	nodes = count_nodes(root);
+	return (is_max_heap(root, 0, nodes));
 }
